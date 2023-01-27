81d103c25d544cda8dc73f88ae828ab785457863
Jānis Strods <janis@gymn.local>
2023-01-27 08:22:31 +0200

    Pirmais

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include "json.hpp"
+
+using namespace std;
+
+using json = nlohmann::json;
+
+int main()
+{
+    ifstream file("test.json");
+
+    json jsonData;
+
+    file >> jsonData;
+
+    cout << jsonData["Patiesuma"] << endl;
+    cout << jsonData["saraksts1"] << endl;
+    cout <<endl;
+    cout << jsonData["ObjSaraksts"]<< endl;
+    cout <<endl;
+
+    cout << "Individualas objekta vertibas!!" << endl;
+
+    for(auto &array : jsonData["ObjSaraksts"])
+    {
+        cout << array["Vertibas1"] << endl;
+        cout << array["Vertibas2"] << endl;
+    }
+    cout <<endl;
+    cout << "Individualas saraksta vertibas!!" << endl;
+
+    for(int i = 0; i < 3; i++)
+    {
+        cout << jsonData["saraksts1"][i] << endl;
+    }
+    cout << jsonData["saraksts1"][1] << endl;
+    cout <<endl;
+    cout <<endl;
+
+    for(auto &elemts : jsonData["saraksts1"].items())
+    {
+        cout << elemts.value() << endl;
+    }
+
+    return 0;
+}
